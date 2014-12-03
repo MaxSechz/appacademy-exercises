93455aed2059dd4bb4e681d8ef7ff9d84577dc02
Max+Justin <Max+Justin@appacademy.io>
2014-12-03 14:49:21 -0500

    Adds associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User<ActiveRecord::Base
-  validates :name, :email, presence: :true
+  validates :user_name, uniqueness: true, presence: :true
+
+  has_many :contact_shares,
+    class_name: 'ContactShare',
+    foreign_key: :user_id,
+    primary_key: :id
+
+  has_many :contacts,
+      class_name: 'Contact',
+      foreign_key: :user_id,
+      primary_key: :id
+
+  has_many :shared_contacts, through: :contact_shares, source: :contact
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141203171854) do
+ActiveRecord::Schema.define(version: 20141203191929) do
 
-  create_table "users", force: true do |t|
-    t.string   "name"
-    t.string   "email"
+  create_table "contact_shares", force: true do |t|
+    t.integer  "contact_id", null: false
+    t.integer  "user_id",    null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "contact_shares", ["contact_id", "user_id"], name: "index_contact_shares_on_contact_id_and_user_id", unique: true
+
+  create_table "contacts", force: true do |t|
+    t.string   "name",       null: false
+    t.string   "email",      null: false
+    t.integer  "user_id",    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "contacts", ["email", "user_id"], name: "index_contacts_on_email_and_user_id", unique: true
+
+  create_table "users", force: true do |t|
+    t.string   "user_name",  null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["user_name"], name: "index_users_on_user_name", unique: true
 
 end
