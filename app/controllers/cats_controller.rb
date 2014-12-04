e77eff28b1a94b2479a3d222b5e6cf499334c6e7
Mark+Max <Mark+Max@email>
2014-12-04 14:49:49 -0500

    Finishes views and model for cats

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -16,6 +16,31 @@ class CatsController < ApplicationController
   end
 
   def create
+    @cat = Cat.new(cat_params)
+    if @cat.save
+      redirect_to cat_url(@cat)
+    else
+      render :new
+    end
   end
 
+  def edit
+    @cat = Cat.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @cat = Cat.find(params[:id])
+    if @cat.update(cat_params)
+      redirect_to cat_url(@cat)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def cat_params
+    params.require(:cat).permit(:name, :color, :birthdate, :description, :sex)
+  end
 end
