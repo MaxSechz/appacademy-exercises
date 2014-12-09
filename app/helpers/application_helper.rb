a1f0e62471eb853988bdf125cc4b013c57683742
Max+Ed <Max+Ed@appacademy.io>
2014-12-09 11:20:01 -0500

    Adds Subs MVCs.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def update_html
+    "<input type=\"hidden\" name=\"_method\" value=\"patch\">".html_safe
+  end
 end
--- app/models/sub.rb
+++ app/models/sub.rb
@@ -4,9 +4,9 @@ class Sub < ActiveRecord::Base
   belongs_to(
     :moderator,
     class_name: 'User',
-    foreign_key: :moderator_id
+    foreign_key: :moderator_id,
     primary_key: :id
   )
-  
+
 
 end
